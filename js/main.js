be148eb0a61149d1ac1e9870525a0288a10c905a
Kemar Norton <kemarnorton@hotmail.com>
2021-12-22 19:19:47 -0500

    Fix linters errors

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const menuIcon = document.querySelector('.menuIcon');
 const menu = document.querySelector('.menu-list');
 const barras = document.querySelectorAll('.menuIcon div');
 
-menuIcon.addEventListener('click', (a) => {
+menuIcon.addEventListener('click', () => {
   menu.classList.toggle('active');
-  barras.forEach(child => { child.classList.toggle('animated'); });
+  barras.forEach((child) => { child.classList.toggle('animated'); });
 });
